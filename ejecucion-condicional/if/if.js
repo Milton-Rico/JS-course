8dcd71e7e6790a8e2a0b931e5cf6c016de85ac92
Milton Rico <miltonrico02@gmail.com>
2025-01-20 09:57:32 -0500

    learn about conditionals

---
+++ ejecucion-condicional/if/if.js
@@ -0,0 +1,17 @@
+// if (let == algo) {
+//   codigo;
+// } else if {
+//   codigo;
+// } else {
+
+// }
+
+let nombre = "Nico";
+
+if (nombre === "Diego") {
+  console.log("Hola Diego");
+} else if (nombre === "Nico") {
+  console.log("Hola Nico");
+} else {
+  console.log("Nombre no encontrado");
+}
---
+++ operadores-logicos/operadores-logicos.js
@@ -0,0 +1,17 @@
+// // Operadores logicos
+// // Si este valor Y este otro
+// &&
+
+// // Si este valor O este otro
+// ||
+
+// // Si este valor NO este otro
+// !
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+console.log(a == b && a === c);
+console.log(a != b || a === c);
+console.log(!(a === c));
